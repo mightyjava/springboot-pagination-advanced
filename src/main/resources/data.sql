c657b9f0c1d8f8f5214f1585a661868346cc3315
mightyjava <jeetparmar949@gmail.com>
2017-11-11 13:28:43 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
-use springboot;
-/*insert into `user`(`user_id`,`user_name`,`password`) values
-('user id 1', 'user name 1', 'password1'),
-('user id 2', 'user name 2', 'password2');*/
+create database springboot;
+
+insert into role(name) values('admin'),('user');
+
+insert into user(email, full_name, mobile_number, password, user_id, user_name, role_id) 
+values('mightyjava@gmail.com', 'Mighty Java', '9876543210', '$2a$10$41qTWvhlE0cKxBWX1RsMLeA9dsWcSzgGPziTKLVBptivQ5Hmu.Pym', 'MJ001', 'mightyjava', 1);
