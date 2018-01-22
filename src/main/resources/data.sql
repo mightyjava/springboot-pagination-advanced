e094b8c72b435b59686b79bf029c3539e3dd80d5
mightyjava <jeetparmar949@gmail.com>
2018-01-22 16:51:46 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-create database springboot;
+#create database mightyjava;
 
-insert into role(name) values('admin'),('user');
+#insert into role(name) values('admin'),('user');
 
-insert into user(email, full_name, mobile_number, password, user_id, user_name, role_id) 
-values('mightyjava@gmail.com', 'Mighty Java', '9876543210', '$2a$10$41qTWvhlE0cKxBWX1RsMLeA9dsWcSzgGPziTKLVBptivQ5Hmu.Pym', 'MJ001', 'mightyjava', 1);
+#insert into user(email, full_name, mobile_number, password, user_id, user_name, role_id) 
+#values('mightyjava@gmail.com', 'Mighty Java', '9876543210', '$2a$10$41qTWvhlE0cKxBWX1RsMLeA9dsWcSzgGPziTKLVBptivQ5Hmu.Pym', 'MJ001', 'mightyjava', 1);
